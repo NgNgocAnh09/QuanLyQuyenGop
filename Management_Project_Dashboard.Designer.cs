ccffeef094386217d921370d3815441c163f3d1d
ngocanh <nguyenngocanh2695@gmail.com>
2025-05-27 14:20:54 +0700

    update

---
+++ Management_Project_Dashboard.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace QuanLyQuyenGop
+{
+    partial class Management_Project_Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Management_Project_Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 450);
+            this.Name = "Management_Project_Dashboard";
+            this.Text = "Management_Project_Dashboard";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Management_Project_Dashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuyenGop
+{
+    public partial class Management_Project_Dashboard : Form
+    {
+        public Management_Project_Dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
